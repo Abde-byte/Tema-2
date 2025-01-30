d5580e511fc954864581289a32872afe64dd1742
arab459 <arab459@g.educaand.es>
2025-01-30 14:21:32 +0100

    Add files via upload

---
+++ Actividad 3/js/script.js
@@ -0,0 +1,12 @@
+function pedirdatos() {
+    mobre=prompt("¿como te llamas?");
+    edad=prompt("¿que edad tienes?");
+    alert("hola soy"+ nombre + "y tengo" + edad + "años")
+    let=nombre
+    let=edad
+}
+
+
+
+
+

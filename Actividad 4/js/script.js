87d8f61443fa39e0349cb0e29740ca65049ade27
arab459 <arab459@g.educaand.es>
2025-01-31 11:27:53 +0100

    Add files via upload

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,10 @@
+function calcular() {  
+    let=a = parseInt(prompt("dime el primer numero"),10);
+    let=b = parseInt(prompt("dime el segundo numero"),10);
+    alert ("el primer numero es:"+ a + "\nEl segundo numero es:" + b + "\nLa suma es:" + (a+b)+ "\nLa resta es:" + (a-b) +"\nLa multiplicacion es:" + (a*b) + "\nLa division es:" + (a/b) + "\nEl resto es:" + (a%b) +"")
+}
+
+
+
+
+
